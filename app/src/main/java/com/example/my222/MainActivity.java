c7ec0ab61f74bc0d525ea694b10b489d589590a6
Art <roliton@yandex.ru>
2025-05-13 22:02:57 +0300

    начало класс заметка

--- app/src/main/java/com/example/my222/MainActivity.java
+++ app/src/main/java/com/example/my222/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.my222;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLOUTdlyaelmentov;
+    private FloatingActionButton buttonPlus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        initViews();
     }
-}
+
+    private void initViews() {
+        linearLOUTdlyaelmentov = findViewById(R.id.LinearLOUTdlyaelmentov);
+        buttonPlus = findViewById(R.id.buttonPlus);
+
+    }
+};
